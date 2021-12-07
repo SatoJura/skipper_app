16e678dd442726e606ee5d01878f1aa265c04e3d
SatoJura <jnnsk0snb@gmail.com>
2021-12-07 14:31:09 +0000

    [Add]javascripts理解した部分へのコメント

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,13 +28,19 @@ $(document).ready(function () {
 
 $(document).ready(function(){
   $("#theTarget").skippr({
+    // スライドショーの変化 ("fade" or "slide")
     transition:"fade",
+    // 変化に係る時間(ミリ秒)
     speed:1000,
     easing:"easeOutQuart",
+    // ナビゲーションの形("block" or "bubble")
     navType:'bubble',
     childrenElementType:'div',
+    // ナビゲーション矢印の表示(trueで表示)
     arrows:true,
+    // スライドショーの自動再生(falseで自動再生なし)
     autoPlay:true,
+    // 自動再生時のスライド切替間隔(ミリ秒)
     autoPlayDuration:3000,
     keyboardOnAlways:true,
     hidePrevious:false
