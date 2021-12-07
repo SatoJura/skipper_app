0b581d2eb50b3f44874b47aa0d25a1e8b8c1e9d5
SatoJura <jnnsk0snb@gmail.com>
2021-12-07 14:25:18 +0000

    [Add]javascriptsの動き

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,3 +25,18 @@ $(document).ready(function () {
     $(this).css('color','red');
   });
 });
+
+$(document).ready(function(){
+  $("#theTarget").skippr({
+    transition:"fade",
+    speed:1000,
+    easing:"easeOutQuart",
+    navType:'bubble',
+    childrenElementType:'div',
+    arrows:true,
+    autoPlay:true,
+    autoPlayDuration:3000,
+    keyboardOnAlways:true,
+    hidePrevious:false
+  });
+});
