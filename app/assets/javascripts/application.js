bd8287f65f799a4add2c88b2105e5633bd008eaa
SatoJura <jnnsk0snb@gmail.com>
2021-12-09 13:41:44 +0000

    [Update]javascripts理解した部分へのコメント

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,17 +32,20 @@ $(document).ready(function(){
     transition:"fade",
     // 変化に係る時間(ミリ秒)
     speed:1000,
+    // easingの種類(動作のパターン)
     easing:"easeOutQuart",
     // ナビゲーションの形("block" or "bubble")
     navType:'bubble',
-    childrenElementType:'div',
+    // 子要素の種類('div' or 'img')。通常は［div］を、スライダーが画像のときは［img］を指定。
+    childrenElementType:'img',
     // ナビゲーション矢印の表示(trueで表示)
     arrows:true,
     // スライドショーの自動再生(falseで自動再生なし)
     autoPlay:true,
     // 自動再生時のスライド切替間隔(ミリ秒)
     autoPlayDuration:3000,
+    // キーボードの矢印キーによるスライド送りの設定(trueで有効)
     keyboardOnAlways:true,
-    hidePrevious:false
+    hidePrevious:true
   });
 });
